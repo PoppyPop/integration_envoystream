68b9ecf46f7cb418a4b33b8b9e71878698f606b6
PoppyPop <skytep@gmail.com>
2026-04-11 15:04:39 +0000

    delete unused

--- custom_components/envoystream/envoy_reader.py
+++ custom_components/envoystream/envoy_reader.py
@@ -13,9 +13,6 @@ from homeassistant.util.network import is_ipv6_address
 
 from .const import LOGGER
 
-LOGIN_URL = "https://enlighten.enphaseenergy.com/login/login.json?"
-TOKEN_URL = "https://entrez.enphaseenergy.com/tokens"
-
 INFO_URL = "https://{}/info.json"
 
 METERS_URL = "https://{}/ivp/meters"
@@ -28,8 +25,6 @@ class EnvoyReader:
     def __init__(
         self,
         host: str,
-        enlighten_user: str | None = None,
-        enlighten_pass: str | None = None,
         enlighten_serial_num: str | None = None,
         enlighten_token: str | None = None,
     ) -> None:
@@ -38,8 +33,6 @@ class EnvoyReader:
         # IPv6 addresses need to be enclosed in brackets
         if is_ipv6_address(self.host):
             self.host = f"[{self.host}]"
-        self.enlighten_user = enlighten_user
-        self.enlighten_pass = enlighten_pass
         self.enlighten_serial_num = enlighten_serial_num
         self.enlighten_token = enlighten_token
         self.firmware_version: str | None = None
@@ -70,15 +63,6 @@ class EnvoyReader:
                 resp.raise_for_status()
 
             if resp.status == 401:
-                if (
-                    self.enlighten_user
-                    and self.enlighten_pass
-                    and self.enlighten_serial_num
-                ):
-                    self.enlighten_token = None
-                    await self._login(http_session)
-                    return await self._async_get(url, http_session, is_retry=True)
-
                 resp.raise_for_status()
 
             if is_json:
@@ -86,61 +70,15 @@ class EnvoyReader:
             else:
                 return await resp.text()
 
-    async def _get_enphase_sessionid(
-        self, http_session: aiohttp.ClientSession, user: str, password: str
-    ) -> str:
-        """Get session_id from login."""
-        data = {"user[email]": user, "user[password]": password}
-        LOGGER.debug("Getting session_id: %s", LOGIN_URL)
-        async with http_session.post(LOGIN_URL, data=data) as resp:
-            resp.raise_for_status()
-            result = await resp.json()
-            return result["session_id"]
-
-    async def _get_enphase_token(
-        self,
-        http_session: aiohttp.ClientSession,
-        envoy_serial: str,
-        user: str,
-        password: str,
-    ) -> str:
-        """Get long-term token."""
-        session_id = await self._get_enphase_sessionid(http_session, user, password)
-        data = {"session_id": session_id, "serial_num": envoy_serial, "username": user}
-        LOGGER.debug("Getting Token: %s", TOKEN_URL)
-        async with http_session.post(TOKEN_URL, json=data) as resp:
-            resp.raise_for_status()
-            jwt_token = await resp.text()
-            self._get_expiry_date(jwt_token)
-            return jwt_token
-
     def _get_expiry_date(self, jwt_token: str) -> None:
         """Decode the token and store its expiry date."""
         decoded_token = jwt.decode(jwt_token, options={"verify_signature": False})
         self._expirydate = datetime.fromtimestamp(decoded_token["exp"], tz=UTC)
         LOGGER.debug("Token expiry date: %s", self._expirydate)
 
-    def _is_token_expired(self) -> bool:
-        return self._expirydate is not None and datetime.now() > self._expirydate
-
-    async def _login(self, http_session: aiohttp.ClientSession) -> None:
-        if (
-            self.enlighten_user
-            and self.enlighten_pass
-            and self.enlighten_serial_num
-            and (self.enlighten_token is None or self._is_token_expired())
-        ):
-            self.enlighten_token = await self._get_enphase_token(
-                http_session,
-                self.enlighten_serial_num,
-                self.enlighten_user,
-                self.enlighten_pass,
-            )
-
     async def get_meters(self, http_session: aiohttp.ClientSession) -> dict[str, str]:
         """Get."""
         if self._meters is None:
-            await self._login(http_session)
             meters = await self._async_get(METERS_URL, http_session)
 
             self._meters = {}
@@ -152,8 +90,6 @@ class EnvoyReader:
 
     async def get_datas(self, http_session: aiohttp.ClientSession) -> dict[str, float]:
         """Fetch data from the endpoint."""
-
-        await self._login(http_session)
         await self.get_meters(http_session)
         assert self._meters is not None
 
